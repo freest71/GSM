5f27f72f825037c8d31d6600db1cf94a9e7675e6
freest71 <31979163+freest71@users.noreply.github.com>
2017-11-05 09:11:42 +0900

    first commit

---
+++ app/src/main/java/com/shb06145620/yyh/gsm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shb06145620.yyh.gsm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
